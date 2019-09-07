eb9beb0ec49a410ec04a776c11fa3812411446cf
Mikael Stockman <micke@stockman.se>
2019-09-07 18:37:31 +0900

    ESP32: Add a beacon light effect.

---
+++ ESP32/BeaconLight.cpp
@@ -0,0 +1,55 @@
+#include "BeaconLight.h"
+#include "LedStrip.h"
+
+
+BeaconLight::BeaconLight(LedStrip* strip) : LampEffect(strip) {
+
+}
+
+void BeaconLight::displayArray() {
+  int maxIndex = this->arrayPos + LedStripConstants::led_count;
+
+  for (int i = 0; i < LedStripConstants::led_count; i++) {
+    int colorIndex = (i + this->arrayPos) % this->colorArrayLength;
+    RgbwColor color = *(this->colorArray[colorIndex]);
+    strip->SetPixelColor(i, color);
+  }
+  strip->Show();
+}
+
+void BeaconLight::setup() {
+  RgbwColor* blue = new RgbwColor(0, 0, 100, 0);
+  RgbwColor* red = new RgbwColor(100, 0, 0, 0);
+
+
+  for (int i = 0; i < this->colorArrayLength;) {
+    for (int j = 0; j < 4; j++, i++) {
+      this->colorArray[i] = blue;
+    }
+    for (int j = 0; j < 4; j++, i++) {
+      this->colorArray[i] = red;
+    }
+    for (int j = 0; j < 4; j++, i++) {
+      this->colorArray[i] = blue;
+    }
+    for (int j = 0; j < 3; j++, i++) {
+      this->colorArray[i] = red;
+    }
+    for (int j = 0; j < 4; j++, i++) {
+      this->colorArray[i] = blue;
+    }
+    for (int j = 0; j < 3; j++, i++) {
+      this->colorArray[i] = red;
+    }
+  }
+
+  displayArray();
+
+}
+
+void BeaconLight::next() {
+  this->arrayPos++;
+  this->arrayPos %= this->colorArrayLength;
+  displayArray();
+  delay(50);
+}
---
+++ ESP32/BeaconLight.h
@@ -0,0 +1,22 @@
+#ifndef BEACON_LIGHT_H
+#define BEACON_LIGHT_H
+
+#include "LampEffect.h"
+#include "LedStrip.h"
+#include <NeoPixelBus.h>
+
+
+class BeaconLight: public LampEffect {
+  private:
+    void displayArray();
+    const static int colorArrayLength = 6 * 22;
+    RgbwColor* colorArray[colorArrayLength];
+    int arrayPos = 0;
+
+  public:
+    BeaconLight(LedStrip* strip);
+    virtual void setup();
+    virtual void next();
+};
+
+#endif
